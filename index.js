7ae2f0a258f8811621e20a0e0e63a28c3ed2f162
Luiz Henrique José Dos Santos <71455360+luizhenriquesantos@users.noreply.github.com>
2023-11-27 22:04:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function somaDiv(num){ // função recebe o parâmetro passado diretamente
+    
+    let total = 0 // contador inicializado em zero
+
+    for (i = 0; i < num; i++ ) { // estrutura que faz a iteração
+        if ( i % 3 == 0 ||  i % 5 == 0){ // condição que verifica se os numeros são divisiveis
+            total += i;
+        }
+
+    }
+    return total;
+}
+
+
+
+const numero = somaDiv(10); // variável (numero) que guarda o valor que a função retorna, chamando a função e passando o valor como parâmetro
+console.log(numero);
