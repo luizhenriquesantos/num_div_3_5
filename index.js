66d46326dca776e1562bfa9e35a9f6356b59d331
Luiz Henrique <luiz.jose@a.unileste.edu.br>
2023-11-27 22:49:20 -0300

    estrutura que verifica e soma todos os número divisível por 3 ou 5, que sejam menores que o valor informado

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function somaDiv(num){ // função recebe o parâmetro passado diretamente
+function somaDiv(num){ // função recebe o parâmetro passado diretamente.
     
-    let total = 0 // contador inicializado em zero
+    let total = 0 // contador inicializado em zero.
 
-    for (i = 0; i < num; i++ ) { // estrutura que faz a iteração
-        if ( i % 3 == 0 ||  i % 5 == 0){ // condição que verifica se os numeros são divisiveis
+    for (i = 0; i < num; i++ ) { // estrutura que faz a iteração.
+        if ( i % 3 == 0 ||  i % 5 == 0){ // condição que verifica se os numeros são divisiveis.
             total += i;
         }
 
@@ -13,5 +13,5 @@ function somaDiv(num){ // função recebe o parâmetro passado diretamente
 
 
 
-const numero = somaDiv(10); // variável (numero) que guarda o valor que a função retorna, chamando a função e passando o valor como parâmetro
+const numero = somaDiv(10); // variável (numero) que guarda o valor que a função retorna, chamando a função e passando o valor como parâmetro.
 console.log(numero);
